9f723df81b7c3c38cc23f0a39051381c75860c04
Javier Holguin <javier@softdev.team>
2018-05-22 04:11:01 -0500

    fix patch

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -39,13 +39,24 @@ export class MovieService {
   }
 
   async _update (MovieData: IUpdateMovie, file: any): Promise<any> {
-      const Movie: Movie = await this.movieRepository.findById(MovieData.id)
-      if (R.isEmpty(Movie)) {
-        throw new Error(`Movie with id '${MovieData.id}' not found`)
-      }
+    const Movie: Movie = await this.movieRepository.findById(MovieData.id)
+    if (R.isEmpty(Movie)) {
+      throw new Error(`Movie with id '${MovieData.id}' not found`)
+    }
 
     const updateData = R.compose(R.mergeDeepRight(Movie), R.omit('id'))
     const data = await this.movieRepository.save(updateData(MovieData))
+    if (file) {
+      fs.writeFile(`${process.cwd()}/photos/${MovieData.id.toString()}.jpg`,file.buffer,'binary', (err) => {
+        if (err) {
+          console.log(err)
+          throw new Error('Fail to upload file')
+        }
+        console.log('The file was saved!')
+      })
+
+    }
+
     return { data }
   }
 
